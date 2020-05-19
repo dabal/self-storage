5d5a69dcbf83d4976ae5340405cb32d5479c3b0a
dabal <dabrowski.grzegorz@o2.pl>
2020-05-19 15:06:49 +0200

    adding initialization of category, quantity, storages

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,3 +1,21 @@
-INSERT INTO roles (id, name) VALUES (1, 'ROLE_USER');
-insert into users (id, username, password, enabled ) values (1,'username', '$2a$10$5kAj3LNwLG3HQbOqN.6Wju3GPf9Rjjqt5yFsUHL3uXx8eYLBNjYFO',true);
-insert into user_role(user_id, role_id) values (1,1);
+INSERT INTO roles (id, name)
+VALUES (1, 'ROLE_USER');
+insert into users (id, username, password, enabled)
+values (1, 'username', '$2a$10$5kAj3LNwLG3HQbOqN.6Wju3GPf9Rjjqt5yFsUHL3uXx8eYLBNjYFO', true);
+insert into user_role(user_id, role_id)
+values (1, 1);
+
+
+insert into storages(id, name, user_id)
+values (1, 'default', 1);
+insert into metrics(id, name)
+values (1, 'Brak'),
+       (2, 'kg'),
+       (3, 'g'),
+       (4, 'l');
+
+insert into categories(id, name)
+values (1, 'Brak'),
+       (2, 'żywność'),
+       (3, 'napoje'),
+       (4, 'chemia');
